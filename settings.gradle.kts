00b43cb5a73c29ac1910122e37fb025e4857c14d
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-10-12 07:08:10 +0200

    Remove wrong plugin repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,3 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven(url = "https://www.jetbrains.com/intellij-repository/releases")
-        maven(url = "https://cache-redirector.jetbrains.com/intellij-dependencies")
-    }
-}
-
 rootProject.name = "postgres-native-sqldelight"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
