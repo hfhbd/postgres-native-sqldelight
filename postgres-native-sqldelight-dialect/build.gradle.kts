b147e78183285b1c8a28ed3bb8e09139032dd87c
hfhbd <hfhbd@users.noreply.github.com>
2023-01-11 12:42:26 +0100

    Bump Gradle to 8.0-rc-1

--- postgres-native-sqldelight-dialect/build.gradle.kts
+++ postgres-native-sqldelight-dialect/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
 }
 
 kotlin {
+    jvmToolchain(17)
+
     target.compilations.all {
         kotlinOptions.allWarningsAsErrors = true
     }
@@ -69,7 +71,7 @@ tasks {
 }
 
 tasks.shadowJar {
-    classifier = ""
+    archiveClassifier.set("")
     include("*.jar")
     include("app/cash/sqldelight/**")
     include("app/softwork/sqldelight/postgresdialect/**")
