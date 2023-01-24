ad90dbdbd8bc07f24dbc2e2f38991e8bd923a542
hfhbd <hfhbd@users.noreply.github.com>
2023-01-24 18:40:10 +0100

    Fix test results
    Fix test results
    
    Use DateTimePeriod for Interval

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,18 @@ pluginManagement {
 
 plugins {
     id("MyRepos")
+    id("com.gradle.enterprise") version "3.12.2"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        if (System.getenv("CI") != null) {
+            publishAlways()
+            tag("CI")
+        }
+    }
 }
 
 rootProject.name = "postgres-native-sqldelight"
